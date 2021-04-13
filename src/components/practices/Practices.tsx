e6a062ba614112c598e44a8e047677705491c341
tayutaedomo <tayutaedomo@gmail.com>
2021-04-13 22:36:28 +0900

    add: Practice3 component

--- src/components/practices/Practices.tsx
+++ src/components/practices/Practices.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Practice1 from './Practice1';
 import Practice2 from './Practice2';
+import Practice3 from './Practice3';
 
 const Practices = () => {
   return (
     <>
       <Practice1 />
       <Practice2 />
+      <Practice3 />
     </>
   );
 };
