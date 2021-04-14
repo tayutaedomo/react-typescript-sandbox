5b7bd0d91bb36be3e07f52cd7c1c0392f1e9af41
tayutaedomo <tayutaedomo@gmail.com>
2021-04-14 13:22:24 +0900

    add: Data fetching logic

--- src/components/custom_hook/CustomHook.tsx
+++ src/components/custom_hook/CustomHook.tsx
@@ -1,17 +1,55 @@
-import React, { VFC } from 'react';
-import UserCard from './UserCard';
+import React, { useState, VFC } from 'react';
+import axios from 'axios';
 
-const user = {
-  id: 1,
-  name: 'Taro',
-  email: 'aaa@example.com',
-  address: 'ADDRESS',
-};
+import { User } from '../../types/api/user';
+import UserCard from './UserCard';
+import { UserProfile } from '../../types/userProfile';
 
 const CustomHook: VFC = () => {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const onClickFetchData = () => {
+    setLoading(true);
+    setError(false);
+
+    axios
+      .get<Array<User>>('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`,
+        }));
+
+        setUserProfiles(data);
+      })
+      .catch((err) => {
+        setError(true);
+        console.error(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   return (
     <div>
-      <UserCard user={user} />
+      <button onClick={onClickFetchData}>Fetch</button>
+      <br />
+      {error ? (
+        <p style={{ color: 'red' }}>Data fetching is failed.</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
     </div>
   );
 };
