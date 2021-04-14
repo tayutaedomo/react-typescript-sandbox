d1d099287b67883421a23f653851731dbf00a0db
tayutaedomo <tayutaedomo@gmail.com>
2021-04-14 16:00:57 +0900

    add: Chakra UI theme

--- src/components/chakraui/ChakraUI.tsx
+++ src/components/chakraui/ChakraUI.tsx
@@ -1,9 +1,11 @@
 import React, { VFC } from 'react';
 import { ChakraProvider, Button } from '@chakra-ui/react';
 
+import theme from './theme/theme';
+
 const ChakraUI: VFC = () => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Button colorScheme="teal">Button</Button>
     </ChakraProvider>
   );
