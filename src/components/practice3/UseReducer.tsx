d3d64cd0f67d88e01a63b4db54a385f442ab714e
tayutaedomo <tayutaedomo@gmail.com>
2021-04-26 21:36:23 +0900

    add: Types to useReducer func

--- src/components/practice3/UseReducer.tsx
+++ src/components/practice3/UseReducer.tsx
@@ -1,14 +1,19 @@
 import { Box, Button, Typography } from '@material-ui/core';
 import React, { useReducer } from 'react';
 
-const initialState = { count: 0 };
+type StateType = { count: number };
+const initialState: StateType = { count: 0 };
 
-function reducer(state, action) {
+type ActionType = { type: 'decrement' | 'increment' | 'reset' };
+
+function reducer(state: StateType, action: ActionType): StateType | never {
   switch (action.type) {
     case 'increment':
       return { count: state.count + 1 };
     case 'decrement':
       return { count: state.count - 1 };
+    case 'reset':
+      return initialState;
     default:
       throw new Error();
   }
@@ -34,6 +39,13 @@ const UseReducer = () => {
           onClick={() => dispatch({ type: 'increment' })}
         >
           +
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => dispatch({ type: 'reset' })}
+        >
+          Reset
         </Button>
       </Box>
     </>
