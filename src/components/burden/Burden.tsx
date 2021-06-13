686267ce3c57cd8321d01d16423b6e67a5383491
tayutaedomo <tayutaedomo@gmail.com>
2021-06-13 16:28:57 +0900

    add: AlbumPhoto handling

--- src/components/burden/Burden.tsx
+++ src/components/burden/Burden.tsx
@@ -1,17 +1,28 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
-import { Album } from '../../types/api/album';
+import { Album, AlbumPhoto } from '../../types/api/album';
 
 const Burden = () => {
   const [albums, setAlbums] = useState<Album[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
+    setLoading(true);
+
     const fetchAlbums = async () => {
       const url = 'https://jsonplaceholder.typicode.com/albums';
-      const albums = (await axios.get<Album[]>(url)).data;
+      const albums = (await axios.get<Album[]>(url)).data.slice(0, 2);
+
+      for (const album of albums) {
+        const url = `https://jsonplaceholder.typicode.com/albums/${album.id}/photos`;
+        const photos = (await axios.get<AlbumPhoto[]>(url)).data.slice(0, 3);
+        console.log(photos);
+        album.photos = photos;
+      }
 
       setAlbums(albums);
+      setLoading(false);
     };
 
     fetchAlbums();
@@ -19,26 +30,33 @@ const Burden = () => {
 
   return (
     <>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>User</th>
-            <th>Title</th>
-          </tr>
-        </thead>
-        <tbody>
-          {albums.map((album) => {
-            return (
-              <tr key={`album${album.id}`}>
-                <td>{album.id}</td>
-                <td>{album.userId}</td>
-                <td>{album.title}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      {!loading && (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Photos</th>
+            </tr>
+          </thead>
+          <tbody>
+            {albums.map((album) => {
+              return (
+                <tr key={`album${album.id}`}>
+                  <td>{album.id}</td>
+                  <td>{album.title}</td>
+                  <td>
+                    {album.photos.map((photo) => {
+                      return <img width={32} src={photo.thumbnailUrl} />;
+                    })}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
+      {loading && <div>Now loading...</div>}
     </>
   );
 };
--- src/types/api/album.ts
+++ src/types/api/album.ts
@@ -2,4 +2,13 @@ export type Album = {
   id: number;
   userId: number;
   title: string;
+  photos: AlbumPhoto[];
+};
+
+export type AlbumPhoto = {
+  id: number;
+  albumId: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
 };
