aeb28a7ccaaf084bc5efb4d4cdf1e1bbd971ab4a
tayutaedomo <tayutaedomo@gmail.com>
2021-07-10 13:43:47 +0900

    refactor: Add UseTimeout component

--- src/components/awesome_hooks/AwesomeHooks.tsx
+++ src/components/awesome_hooks/AwesomeHooks.tsx
@@ -1,23 +1,12 @@
 import React, { VFC } from 'react';
-import { Box, Typography } from '@material-ui/core';
 
-import { useTimeout } from './hooks/useTimeout';
-import { useState } from 'react';
+import UseTimeout from './UseTimeout';
 
 const AwesomeHooks: VFC = () => {
-  const [nowText, setNowText] = useState<string>(new Date().toISOString());
-
-  useTimeout(() => {
-    const text = new Date().toISOString() + ', Updated';
-    console.log(text);
-    setNowText(text);
-  }, 2000);
-
   return (
-    <Box py={2}>
-      <Typography variant="h4">useTimeout</Typography>
-      <Typography>{nowText}</Typography>
-    </Box>
+    <>
+      <UseTimeout />
+    </>
   );
 };
 
